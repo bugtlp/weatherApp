a80d17c3bc54558940970a498a2ceab7127a32d3
Anatoliy Kireev <bug.tlp@gmail.com>
2018-07-04 00:12:43 +0300

    autoload cities

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { render } from 'react-dom';
 import { useStrict } from 'mobx';
+import { Provider } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
-
+import CitiesStore from 'stores/CitiesStore';
 import MainPage from 'pages/MainPage';
 
 useStrict(true);
 
+const citiesStore = new CitiesStore(window.localStorage);
+
 render(
-  <div>
-    <DevTools />
-    <MainPage />
-  </div>,
+  <Provider cities={citiesStore}>
+    <div>
+      <DevTools />
+      <MainPage />
+    </div>
+  </Provider>,
   document.getElementById('root'),
 );

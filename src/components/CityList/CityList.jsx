17f7cab537826a777a439d0efa5b1b2c7e130013
Anatoliy Kireev <bug.tlp@gmail.com>
2018-07-04 00:20:54 +0300

    created component of cities

---
+++ src/components/CityList/CityList.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import {
+  shape as shapeType,
+  string as stringType,
+  func as funcType,
+} from 'prop-types';
+import { observable, action } from 'mobx';
+import { observer, PropTypes as MobxPropTypes } from 'mobx-react';
+
+import WeatherStore from 'stores/WeatherForecastStore';
+import service from 'services/weatherService';
+
+import css from './CityList.css';
+
+@observer
+class CityList extends Component {
+  @observable cityName = '';
+
+  static propTypes = {
+    store: shapeType({
+      cityNames: MobxPropTypes.arrayOrObservableArrayOf(stringType),
+      addCity: funcType,
+      removeCity: funcType,
+    }).isRequired,
+  }
+
+  componentWillMount() {
+    const { store } = this.props;
+    this.store = store;
+  }
+
+  @action
+  onInputChange = (e) => {
+    this.cityName = e.target.value;
+  };
+
+  @action
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.store.addCity(this.cityName);
+    // const store = new WeatherStore({ name: this.cityName }, {
+    //   fetch: service.getForecast,
+    // });
+    this.cityName = '';
+  };
+
+  onDelete = (e) => {
+    e.preventDefault();
+    const cityName = e.target.parentNode.getAttribute('data-name');
+    this.store.removeCity(cityName);
+  }
+
+  onSelect = (e) => {
+    const cityName = e.target.parentNode.getAttribute('data-name');
+    this.store.setSelected(cityName);
+  }
+
+  render() {
+    const { onDelete, onSelect } = this;
+    const { cityNames } = this.store;
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          Новый город:
+          <input
+            type="text"
+            value={this.cityName}
+            onChange={this.onInputChange}
+          />
+          <button type="submit">
+            Добавить
+          </button>
+        </form>
+        <div className={css.cityList}>
+          {cityNames.map(city => (
+            <div key={city} data-name={city}>
+              <span onClick={onSelect}>
+                {city}
+              </span>
+              <button type="button" onClick={onDelete}>
+                x
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CityList;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
+import {
+  inject,
+  observer,
+  PropTypes as MobxPropTypes,
+} from 'mobx-react';
+import PropTypes from 'prop-types';
 
-import TodoList from 'components/TodoList';
+import CityList from 'components/CityList';
+import WeatherWidget from 'components/WeatherWidget';
 
 import WeatherForecastStore from 'stores/WeatherForecastStore';
 import service from 'services/weatherService';
 
+@inject('cities')
+@observer
 export class MainPage extends Component {
+  static propTypes = {
+    cities: PropTypes.shape({
+      cityNames: MobxPropTypes.arrayOrObservableArrayOf(PropTypes.string),
+    }).isRequired,
+  }
+
   componentWillMount() {
-    this.store = new WeatherForecastStore({ name: 'London' }, {
+    this.weatherStore = new WeatherForecastStore({ name: 'London' }, {
       fetch: service.getForecast,
     });
   }
 
   componentDidMount() {
-    const { store } = this;
-    store.load();
-    // TODO: код инициализации городов по умолчанию
+    const { cities } = this.props;
+    cities.load();
   }
 
   render() {
-    const { store } = this;
+    const { cities } = this.props;
     return (
       <div>
-        <TodoList store={store} />
+        <CityList store={cities} />
+        <hr />
+        <WeatherWidget
+          store={this.weatherStore}
+        />
       </div>
     );
   }

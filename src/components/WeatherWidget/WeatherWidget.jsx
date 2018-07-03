8ae9484ba5c8918a93e03b8013020ce6f562489e
Anatoliy Kireev <bug.tlp@gmail.com>
2018-07-03 17:28:14 +0300

    switching temperature mode

--- src/components/WeatherWidget/WeatherWidget.jsx
+++ src/components/WeatherWidget/WeatherWidget.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   shape as shapeType,
   string as stringType,
+  func as funcType,
 } from 'prop-types';
 import { observer } from 'mobx-react';
 import css from './WeatherWidget.css';
@@ -13,28 +14,36 @@ export class WeatherWidget extends Component {
       placeName: stringType,
       temperature: stringType,
       description: stringType,
+      toggleMode: funcType,
     }).isRequired,
+  }
+
+  componentWillMount() {
+    const { store } = this.props;
+    this.store = store;
   }
 
   onDelete = () => {
     console.log('delete city');
   }
 
+  onTempClick = () => {
+    this.store.toggleMode();
+  }
+
   render() {
     const {
-      store: {
-        placeName,
-        temperature,
-        description,
-      },
-    } = this.props;
-
+      placeName,
+      temperature,
+      description,
+    } = this.store;
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
     return (
       <div className={css.wrapper}>
         <div>
           {placeName}
         </div>
-        <div>
+        <div onClick={this.onTempClick} onKeyPress={this.onTempClick}>
           {temperature}
         </div>
         <div>
